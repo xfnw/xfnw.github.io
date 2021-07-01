456ac0d4b3d64d6c849dce94d126a2ee9822c734
xfnw <xfnw@riseup.net>
2021-07-01 12:32:30 -0400

    add some more options to s.php

--- badge/s.php
+++ badge/s.php
@@ -1,26 +1,30 @@
 <?php
-if (isset($_GET['t']) && isset($_GET['c'])) {
-$title = $_GET['t'];
-$content = $_GET['c'];
-$len = (strlen($title . $content)*9)+20;
-$offset = (strlen($title)*9)+10;
+function badge($title,$content,$status) {
+	$len = (strlen($title . $content)*9)+20;
+	$offset = (strlen($title)*9)+10;
+	
+	
+	$img = imagecreate($len, 30);
+	
+	$textbgcolor = imagecolorallocate($img, 30, 30, 32);
+	$textcolor = $status == 0 ? imagecolorallocate($img, 70, 255, 155) : imagecolorallocate($img, 255, 95, 69);
+	
+	imagestring($img, 5, 5, 5, $title, $textcolor);
+	imagefilledrectangle($img, $offset, 0, 2000, 30, $textcolor);
+	imagestring($img, 5, $offset + 5, 5, $content, $textbgcolor);
+	
+	ob_start();
+	imagepng($img);
+}
 
-// Set the content-type
-header('Content-type: image/png');
-
-$img = imagecreate($len, 30);
-
-    $textbgcolor = imagecolorallocate($img, 30, 30, 32);
-    $textcolor = imagecolorallocate($img, 70, 255, 155);
-
-imagestring($img, 5, 5, 5, $title, $textcolor);
-imagefilledrectangle( $img, $offset, 0, 2000, 30, $textcolor );
-imagestring($img, 5, $offset + 5, 5, $content, $textbgcolor);
-
-        ob_start();
-        imagepng($img);
-} else {
-echo "error<br>";
-echo "please set the `t` and `c` query perams to use this";
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) {
+	// Set the content-type
+	header('Content-type: image/png');
+	
+	if (isset($_GET['t']) && isset($_GET['c'])) {
+		badge($_GET['t'], $content = $_GET['c'], isset($_GET['s']) ? $_GET['s'] : 0) ;
+	} else {
+		badge('badge','error',1);
+	}
 }
 ?>
